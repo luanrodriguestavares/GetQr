e5f43e3a91b0e0d5e4c7119276bf5dc964cefce0
Luan Rodrigues <luanrodrigues.contatoprofissional@outlook.com>
2024-03-26 21:14:29 -0300

    cam updated

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -26,7 +26,7 @@ function Card() {
                 <p className="text-indigo-200 text-xs xl:text-lg mb-10 text-center">Maximizing Data Retrieval with QR Ease</p>
                 <div className="flex justify-center">
                     <div className="flex items-center">
-                        <video ref={videoRef} style={{ display: 'none' }}></video>
+                        <video ref={videoRef}></video>
                         <button type="button" onClick={startScanner} className="flex items-center text-gray-900 bg-indigo-50 focus:outline-none transition-transform duration-300 hover:scale-110 focus:ring-4 focus:ring-indigo-300 font-medium rounded-full text-sm px-5 py-2.5">
                             <Camera className="mr-2" />
                             Scan
